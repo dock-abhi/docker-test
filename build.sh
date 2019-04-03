fa31a6dbd77ebb7bcfeab717ab857505f6226014
dock-abhi <47770375+dock-abhi@users.noreply.github.com>
2019-04-03 14:16:24 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /opt/pin/BRM
+source source.me.sh
+cd /opt/pin/BRM/source/sys/fm_cust_pol
+make clean
+make
+cp fm_cust_pol_custom.so /opt/pin/BRM/lib/fm_cust_pol_custom.so
+stop_cm
+start_cm
+echo "#--------Done updating the Code------------------#"
